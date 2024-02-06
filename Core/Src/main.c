283bf3e35c7cae5edc63ac6df5a75896d413f3af
apbashara <alexbashara2002@gmail.com>
2024-02-06 09:21:51 -0600

    Updated WIP Blinky

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,40 +1,19 @@
 #include "stm32h743xx.h"
-#include "system_stm32h7xx.h"
 #include "main.h"
 
-/**
- * @brief Initialize the GPIO pins
- * @note Need PB0 and PE1 for LEDs
- */
-void GPIO_Init() {
-  // Enable GPIO port B clock
-  RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN | RCC_AHB4ENR_GPIOEEN;
-
-  GPIOB->MODER |= GPIO_MODER_MODE0_0; // Set GPIO mode Output
-}
-
-/**
- * @brief Blink the LED
- * @note Uses bitwise XOR to toggle the LED
- */
-void LED_Blink() {
-  // Toggle PB0
-  GPIOB->ODR ^= GPIO_ODR_OD5;
-  GPIOE->ODR ^= GPIO_ODR_OD1;
-}
-
-/**
- * @brief main function to initialize the system
- * @note Contains the infinite loop
- * @return int 
- */
 int main(void) {
-  GPIO_Init();
+  RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN; // Enable GPIOB clock
+
+  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE8_Pos);
+  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE9_Pos);
+  GPIOB->OTYPER &= ~GPIO_OTYPER_OT8;
+  GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;
 
   while (1) {
-    LED_Blink();
+    GPIOB->ODR ^= GPIO_ODR_OD8;
+    GPIOB->ODR ^= GPIO_ODR_OD9;
     for (int i = 0; i < 1000000; i++) {
-      __NOP();
+      __asm__("nop");
     }
   }
 }
