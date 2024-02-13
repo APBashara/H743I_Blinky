bef03ac11364f085f447d13c93bf1bc221e2e6eb
apbashara <alexbashara2002@gmail.com>
2024-02-12 21:36:15 -0600

    Updated Blinky and Cortex-Debug

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -4,16 +4,24 @@
 int main(void) {
   RCC->AHB4ENR |= RCC_AHB4ENR_GPIOBEN; // Enable GPIOB clock
 
-  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE8_Pos);
-  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE9_Pos);
-  GPIOB->OTYPER &= ~GPIO_OTYPER_OT8;
-  GPIOB->OTYPER &= ~GPIO_OTYPER_OT9;
+  // LD1 (green): GPIOB pin 0
+  // LD2 (blue): GPIOB pin 7
+  // LD3 (red): GPIOB pin 14
+
+  // Set to output mode
+  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE0_Pos);
+  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE7_Pos);
+  GPIOB->MODER |= (0x1 << GPIO_MODER_MODE14_Pos);
+  // set to push-pull output mode
+  GPIOB->OTYPER &= GPIO_OTYPER_OT0_Msk;
+  GPIOB->OTYPER &= GPIO_OTYPER_OT7_Msk;
+  GPIOB->OTYPER &= GPIO_OTYPER_OT14_Msk;
+
 
   while (1) {
-    GPIOB->ODR ^= GPIO_ODR_OD8;
-    GPIOB->ODR ^= GPIO_ODR_OD9;
-    for (int i = 0; i < 1000000; i++) {
-      __asm__("nop");
-    }
+    GPIOB->ODR ^= GPIO_ODR_OD0;
+    GPIOB->ODR ^= GPIO_ODR_OD7;
+    GPIOB->ODR ^= GPIO_ODR_OD14;
+    HAL_Delay(100);
   }
 }
